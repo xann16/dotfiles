965d05ddef743beb1834c14e94305b8b84916f64
Maciej Manna <maciejmanna@gmail.com>
2025-04-24 02:21:11 +0200

    slightly adjusted powerline10k colors - fixed

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -185,7 +185,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=62
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=61
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
@@ -215,7 +215,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=62
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=61
   # Default current directory foreground color.
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=7
   # If directory is too long, shorten some of its segments to the shortest possible unique
